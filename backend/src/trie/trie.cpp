de22b2e498b7b65b6fb8233f565e6020c991a147
Hieu <hieunguyen.0527@gmail.com>
2024-07-20 23:57:46 -0500

    finish naive trie impl

--- backend/src/trie/trie.cpp
+++ backend/src/trie/trie.cpp
@@ -12,7 +12,7 @@ Trie::Trie(string root)
 }
 
 // TODO: do this recursively
-void Trie::insert(string path)
+void Trie::insert(string path, void *handler)
 {
     if (path == "/") {
         // TODO
@@ -23,10 +23,10 @@ void Trie::insert(string path)
     Node *currentNode = this->root;
     for (auto p : path_segments) {
         bool pathAlreadyExits = false;
-        
-        // this doesn't seem right?
+
         for (auto c : currentNode->getChildren()) {
             if (c->path == p) {
+                currentNode = c;
                 pathAlreadyExits = true;
                 break;
             }
@@ -39,13 +39,94 @@ void Trie::insert(string path)
         currentNode->addChild(newNode);
         currentNode = newNode;
     }
+
+    currentNode->setValue(handler);
 }
 
-void Trie::find(string path) {}
+Node *Trie::find(string path)
+{
+    auto path_segments = utils::split_str(path, "/");
 
-void Trie::remove(string path) {}
+    Node *currentNode = this->root;
+    for (auto p : path_segments) {
+        bool found = false;
 
-void Trie::display(Node* n)
+        for (auto c : currentNode->getChildren()) {
+            if (c->path == p && c->isTerminal()) {
+                currentNode = c;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            return nullptr;
+        }
+    }
+
+    return currentNode;
+}
+
+// find a cleaner way of doing this without having to use targetPath
+Node *Trie::_remove(Node *n, string targetPath, vector<string> &paths,
+                    int index)
+{
+    // if (index-1 >= paths.size()) {
+    //     std::cout << "index: " << index << endl;
+    //     std::cout << n->path << endl;
+    //     return nullptr;
+    // }
+
+    if (n == nullptr) {
+        return nullptr;
+    }
+
+    if (n->path == targetPath) {
+        if (!(n->isTerminal())) {
+            return nullptr;
+        }
+
+        if (!(n->getChildren().empty())) {
+            n->setValue(nullptr);
+        }
+        else {
+            delete n;
+            return n;
+        }
+    }
+    else {
+        Node *removed = nullptr;
+        auto children = n->getChildren();
+        for (int i = 0; i < children.size(); i++) {
+            if (children[i]->path == paths[index]) {
+                removed =
+                    this->_remove(children[i], targetPath, paths, index + 1);
+                if (removed) {
+                    // why can't we not do: children.erase(...) ??
+                    // children is just a reference to the original children, but for some reason
+                    // the changes are not reflected in the original one
+                    n->children.erase(n->children.begin() + i);
+                }
+                
+                if (n->children.empty() && !n->isTerminal()) {
+                    delete n;
+                    return n;
+                }
+                break;
+            }
+        }
+    }
+
+    return nullptr;
+}
+
+void Trie::remove(string path)
+{
+    auto paths = utils::split_str(path, "/");
+    auto target = paths.back();
+    _remove(this->root, target, paths, 0);
+}
+
+void Trie::display(Node *n)
 {
     Node *currentNode = n;
 
@@ -54,7 +135,6 @@ void Trie::display(Node* n)
     }
 
     std::cout << currentNode->path << std::endl;
-
     for (auto c : currentNode->getChildren()) {
         this->display(c);
     }
@@ -66,12 +146,26 @@ Node::Node(string path, void *handler)
     this->handler = handler;
 }
 
+bool Node::isTerminal()
+{
+    if (this->handler) {
+        return true;
+    }
+
+    return false;
+}
+
 void Node::addChild(Node *n)
 {
     this->children.push_back(n);
 }
 
-vector<Node *> Node::getChildren()
+vector<Node *> &Node::getChildren()
 {
     return this->children;
+}
+
+void Node::setValue(void *n)
+{
+    this->handler = n;
 }
--- backend/src/trie/trie.h
+++ backend/src/trie/trie.h
@@ -4,23 +4,27 @@ using namespace std;
 
 class Node {
     void* handler;
-    bool isTerminal;
-    vector<Node*> children; 
     
   public:
     string path;
+    // what data structure should we use to hold the children?
+    vector<Node*> children; 
+    // bool isTerminal;
 
     Node(string path = "/", void* handler = nullptr);
     void addChild(Node*);
-    vector<Node*> getChildren();
+    void setValue(void*);
+    bool isTerminal();
+    vector<Node*>& getChildren();
 };
 
 class Trie {
     Node* root;
+    Node* _remove(Node* n, string targetPath, vector<string>& paths, int index);
   public:
     Trie(string root);
-    void find(string path);
-    void insert(string path);
+    Node* find(string path);
+    void insert(string path, void* handler);
     void remove(string path);
     void display(Node* n = nullptr);
 };
