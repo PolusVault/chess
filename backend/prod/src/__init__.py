15445ea5b3ef9a914c668835ffbcae36d7a6928d
Hieu <hieunguyen.0527@gmail.com>
2024-08-05 15:51:33 -0500

    temp fix

--- backend/prod/src/__init__.py
+++ backend/prod/src/__init__.py
@@ -8,18 +8,19 @@ socketio = SocketIO(path="/chess/socket")
 logger = logging.getLogger("werkzeug")
 logger.setLevel(logging.ERROR)
 
-
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_prefixed_env()
+    # app.config.from_prefixed_env()
+    #
+    # if app.config["ENV"] == "dev":
+    #     if test_config is None:
+    #        app.config.from_file("config.dev.toml", load=tomllib.load, text=False)
+    #     else:
+    #         app.config.from_mapping(test_config)
+    # else:
+    # app.config.from_file("config.prod.toml", load=tomllib.load, text=False)
 
-    if app.config["ENV"] == "dev":
-        if test_config is None:
-            app.config.from_file("config.dev.toml", load=tomllib.load, text=False)
-        else:
-            app.config.from_mapping(test_config)
-    else:
-        app.config.from_file("config.prod.toml", load=tomllib.load, text=False)
+    app.config.from_file("config.dev.toml", load=tomllib.load, text=False)
 
     try:
         os.makedirs(app.instance_path)
